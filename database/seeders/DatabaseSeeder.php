5642d42be9a41226101f5e3cdda48fecc7718746
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-23 00:00:15 +0300

    Init Project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        if (!User::first())
+        {
+            User::factory()->create([
+                'name' => 'Admin User',
+                'email' => 'admin@aldiwan.net',
+            ]);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+\Livewire\Livewire::setScriptRoute(function ($handle) {
+    return Route::get('/aldiwan/livewire/livewire.js', $handle);
+});
+
+\Livewire\Livewire::setUpdateRoute(function ($handle) {
+    return Route::post('/aldiwan/livewire/update', $handle);
+});
